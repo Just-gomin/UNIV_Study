187534292757d16f72257adf0c3dd97d0167c2f7
Kangmin An <hkkm612@gmail.com>
2021-03-19 12:32:32 +0900

    응용수학 1차 과제 Feedback

--- AppliedMath/homework/1stHW/01.py
+++ AppliedMath/homework/1stHW/01.py
@@ -6,3 +6,6 @@ B = np.array([[1, 0], [0, 1], [1, 0]])
 print("a. 2A \n {0}\n".format(2*A))
 print("b. A의 각 성분의 제곱 \n {0}\n".format(A**2))
 print("c. A*B \n {0}\n".format(np.matmul(A, B)))
+
+# np.matmul 보다 dot() method 이용
+print("c. A*B \n {0}\n".format(A.dot(B)))
--- AppliedMath/homework/1stHW/05.py
+++ AppliedMath/homework/1stHW/05.py
@@ -4,6 +4,9 @@ A = np.array([[-1, 2, -5.4], [0, 1, -100.5]])
 
 print('Before A\n{0}'.format(A))
 
-A[np.where(A < 0)] = 0
+# A[np.where(A < 0)] = 0
+
+# 간단히 A[A<0]을 이용해 표현
+A[A < 0] = 0
 
 print('After A\n{0}'.format(A))
