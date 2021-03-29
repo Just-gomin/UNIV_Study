3c3c0e190453cb15e6bcc5119edbd1c92fe9c45c
Kangmin An <hkkm612@gmail.com>
2021-03-29 16:50:42 +0900

    과제 형식 생성

---
+++ AppliedMath/과제 파일 형식/solution.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def inputLayer(x1, x2):
+    pass
+
+
+def hiddenLater(a1, a2):
+    pass
+
+
+def solution(x1, x2):
+    pass
+
+
+exTable = []
+
+print("{0:>4} {1:>4} {2:>4}".format('x1', 'x2', 'y'))
+for x1, x2 in exTable:
+    print("{0:>4} {1:>4} {2:>4}".format(x1, x2, solution(x1, x2)))
